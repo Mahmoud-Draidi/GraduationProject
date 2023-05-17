7f09c5b8613f6ab884d5d3fc81a5ce9bd4363ef3
Mahmoud-Draidi <almosale.u@gmail.com>
2023-05-17 13:53:08 +0300

    Task : Open to Home or Sign in, 5th day

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mowasulatuna/providers/input_box_provider.dart';
 import 'package:mowasulatuna/providers/sign_in_provider.dart';
 import 'package:mowasulatuna/providers/sned_code_provider.dart';
-import 'package:mowasulatuna/screens/common_screens/easy_booking_screen.dart';
 import 'package:mowasulatuna/screens/common_screens/easy_screen.dart';
-import 'package:mowasulatuna/screens/common_screens/know_screen.dart';
 import 'package:mowasulatuna/screens/common_screens/logo_screen.dart';
 import 'package:mowasulatuna/screens/common_screens/sign_in_screen.dart';
-import 'package:mowasulatuna/screens/rider_screens/send_code_screen.dart';
-import 'package:mowasulatuna/screens/rider_screens/sign_up_screen.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:mowasulatuna/screens/rider_screens/r_home.dart';
 
 // import 'package:firebase_core/firebase_core.dart';
 // import 'firebase_options.dart';
 
 int? initScreen;
+
+bool? isSignedIn;
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +30,14 @@ Future<void> main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  var user = FirebaseAuth.instance.currentUser;
+  if(user == null){
+    isSignedIn = false;
+  }else{
+    isSignedIn = true;
+  }
+
   runApp(
     /// Providers are above [MyApp] instead of inside it, so that tests
     /// can use [MyApp] while mocking the providers
@@ -60,7 +68,9 @@ class MyApp extends StatelessWidget {
 
       initialRoute: initScreen == 0 || initScreen == null ? 'intro' : 'home',
       routes: {
-        'home' : (context) => SignInScreen(),
+
+        // Mustttttttttttttttttttttttt if driver or rider RHome() Or MyBus()
+        'home' : isSignedIn == false ? (context) => SignInScreen() : (context) => RHome(),
         'intro' : (context) => EasyScreen(),
       },
     );
